4c5e0db672658dcbe125c540b1460b583d23e845
webdfabinho <webdfabinho@gmail.com>
2021-10-05 17:47:16 -0300

    Exemplos Variaveis e Tipo

--- index2.js
+++ index2.js
@@ -2,8 +2,11 @@ var firstName = 'João';
 var lastName = 'Souza';
 
 if(firstName === 'João'){
+    let lastName = 'Rodrigues'
     var firstName = 'Pedro';
     let lastName = 'Silva';  
+
+    console.log(lastName);
 }
 
 console.log(firstName, lastName);
